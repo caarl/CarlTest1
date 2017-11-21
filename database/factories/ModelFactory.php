d7c0e20b1f3c3a061f23b51447dc5d335f464d40
Carl Simpson <carl.simpson@zeninternet.co.uk>
2017-11-21 16:29:46 +0000

    Revert "replaced with sample project"
    This reverts commit 44d8bd301999ae520f0c4e6983c99a72634be2bb.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,8 +11,13 @@
 |
 */
 
-$factory->define(App\Task::class, function (Faker\Generator $faker) {
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+
     return [
-        'name' => $faker->title,
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
     ];
 });
